db2a778f18e14339ace617d2bb7b0a73ff0d12bf
bertocarl <aomware@gmail.com>
2019-02-11 08:04:30 +0300

    made the start.sh file

---
+++ start.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY="bertocarl"
+
+export MAIL_USERNAME="aomware@gmail.com"
+export MAIL_PASSWORD="fiddle"
+
+export DATABASE_URL='postgresql+psycopg2://berto:fiddle@localhost/pitch@60'
+
+python3.7 manage.py server
