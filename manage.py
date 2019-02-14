1ba0e1e8795a5d1dd986eab0a3a2e64ec0b40eee
bertocarl <aomware@gmail.com>
2019-02-14 11:40:29 +0300

    made changes in the database

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User,Role,Blog,Comment,Category,Subscribe
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 
 migrate = Migrate(app,db)
