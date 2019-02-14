fdeb6928edb6ebe18d1d6277d71b4f17e8a293cf
bertocarl <aomware@gmail.com>
2019-02-14 08:23:14 +0300

    made changes with the styling

--- app/models.py
+++ app/models.py
@@ -18,12 +18,7 @@ class User(UserMixin,db.Model):
     name = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     password_hash = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    the_blog = db.relationship('Blog', backref='user', lazy='dynamic')
-
-    # comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
-
+      
     @property
     def password(self):
          raise AttributeError('You cannot read the password attribute')
@@ -44,7 +39,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    # users = db.relationship('User',backref = 'role',lazy="dynamic")
+   
 
 
 class Blog(db.Model):
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = "elias"
+    SECRET_KEY ='bertocarl'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
